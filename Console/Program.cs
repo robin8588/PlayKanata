a0d49e7f04c7adda1944a59a412f950708ec6b9f
leole8588 <leole8588@hotmail.com>
2016-04-28 10:09:51 +0800

    inital

---
+++ Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<WebAPI.Startup>("http://localhost:5000"))
+            {
+                Console.WriteLine("running");
+                Console.ReadLine();
+            };
+        }
+    }
+}
---
+++ WebAPI/App_Start/Startup.Route.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+
+
+namespace WebAPI
+{
+    public partial class Startup
+    {
+        public void ConfigurationRoute(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            // Web API 配置和服务
+            // 将 Web API 配置为仅使用不记名令牌身份验证。
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
